f3243c5e26cd184a685a7ea42e465ca2744b96c7
boca5tudor <boca_tudor@yahoo.com>
2017-02-06 11:08:09 +0000

    Add files via upload

---
+++ GelMurTrans/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="GelMurTrans">
+    <meta name="author" content="Life">
+
+	<title>GelMurTrans</title>
+
+
+	  <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
+
+
+     <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+
+	  <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Theme CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+         	<!-- My CSS -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" >
+
+
+
+</head>
+<body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    Meniu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-play-circle"></i> <span class="light">GelMurTrans</span> 
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Portofoliu</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#otherproj">Utilaje</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+        <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+        
+            <div class="container">
+                <div class="row">
+                
+                    <div class="col-md-8 col-md-offset-2">
+                    
+                        <h4 class="brand-heading">GelMurTrans</h4>
+                        <p class="intro-text">S.C. Gelmurtrans S.R.L. este o companie romaneasca ,tanara si dinamica infiintata in anul 2005 cu capital integral privat avand ca obiect principal  de activitate lucrari de contructii specifice infrastructurii de drumuri.Obiectivul nostru este intotdeauna realizarea constructiilor de calitate si durabile in timp, oferind clientilor economie de timp si bani,deasemenea venim in intampinarea proiectelor dumneavoastra cu solutii profesionale la preturi avantajoase si competitive.
+                            <br>Muresan Eugen</p>
+                            
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                        
+                </div>
+            </div>                   
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Ca si portofoliu de lucari amintim:</h2>
+                <p>
+-reabilitarea drum european  Livada-Cuzrioara-Fcc Construction-executie rigole,taluzari,excavatii casete de drum.</p>
+<p>
+-reabilitare drum european E58 Dej-Baia-Mare-Fcc Construction-executie rigole,sant trapezoidal,triunghiular,rogol a carosabila,excavatii caseta de drum,taluzari,santuri de pamant,excavatii amenajari albii poduri si podete.
+</p>
+<p>
+-reabilitare cale ferata lot 2 Atel-Micasasa-FCC Construction& AZVI-executie podete cale ferata,excavatii amenajari albii poduri si podete.</p>
+<p>
+-reabilitare drum comunal Livezile jud Bistrita-DPJ Cluj-executie casete de drum,santuri de pamant trapezoidale.</p>
+<p>
+-reabilitare drum judetean Fodora-Valea Grosilor jud Salaj-DPJ Cluj-executie casete de drum,santuri de pamant trapezoidale.</p>
+<p>
+-reabilitare drum forestier Barlogul Mare(jud Hunedoara) beneficiar Romsilva.</p>
+<p>
+-demolare placi calcar-cariera Macau jud Cluj-Decorastone-producator piatra naturala.
+</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Other Projects Section -->
+    <section id="otherproj" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Utilaje</h2>                    
+                </div>
+            </div>
+
+            <!-- Reference: https://github.com/ashleydw/lightbox/
+<script src="//rawgithub.com/ashleydw/lightbox/master/dist/ekko-lightbox.js"></script>    -->
+
+<div class="container mt40">
+    
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    
+                        <img src="img/Liebherr912.jpg" alt="Liebherr 912" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                <div class="panel-footer">
+                    <h4><a href="#" title="Nature Portfolio">Liebherr 912 hdsl(senilat)-25 tone-dotat cu cupa de excavatie-1,2m3,cupa de taluz inclinabila,picon demolare 2000kg(nou).</a></h4>
+                                  
+                    </span>
+                </div>
+            </div>
+        </article>
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    
+                        <img src="img/Liebherr902.jpg" alt="Liebherr 902" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                <div class="panel-footer">
+                    <h4><a href="#" title="Food Portfolio">Liebherr 902(pneuri)-18tone-dotat cu cupa de excavatie 0,800m3,cupa de taluz telescopica.</a></h4>
+                 
+                </div>
+            </div>
+        </article>
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                   
+                        <img src="img/AutobasculantaMAN.jpg" alt="Autobasculanta MAN" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                <div class="panel-footer">
+                    <h4><a href="#" title="Sports Portfolio" title="Sports">Autobasculanta MAN 32 tone cu bena de 18m3 </a></h4>  
+                   
+                </div>
+            </div>
+        </article>
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                   
+                        <img src="img/KomatsuPC50.jpg"  alt="KomatsuPC50" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                <div class="panel-footer">
+                    <h4><a href="#" title="People Portfolio">Miniexcavator komatsu PC 50 MR2-cupa de excavatie si taluz.</a></h4>  
+                    
+                </div>
+            </div>
+        </article>                                              
+
+</div>
+        </div>
+    </section>
+
+    <section id="contact" class="container content-section text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+			<h2>Contact</h2>
+				<hr class="marginbot-50">
+			</div>
+		</div>
+     
+		<div class="col-lg-12">
+			<div class="widget-contact">			
+				
+				<address id="myaddress" class="myaddress">
+				<strong>Muresan Eugen</strong>  <br>
+				  Str. Clujului nr.4 A<br>
+				  Livada, Jud. Cluj<br>
+				  <abbr title="Telefon">Tel:</abbr> +40 0744203869
+                  <br>
+                  <abbr title="Telefon">Tel:</abbr> +40 0754377204
+				</address>
+
+				<address id="myemail" class="myemail">
+				  <strong>Email</strong><br>
+				  <a id="eMal" href="mailto:boca_tudor@yahoo.com">gelmurtrans2005@gmail.com</a>
+				</address>				
+			
+			</div>	
+		</div>
+    
+
+		</div>
+</section>
+
+        <footer>
+        <div class="container text-left">
+            <p>Copyright &copy; GelMurTrans 2017</p>
+        </div>
+    </footer>
+
+</body>
+</html>
